eb035bb6a2a971bbdff177f6e105969ffc67f491
Pulkit Khandelwal <67990422+pulkitkhandelwal29@users.noreply.github.com>
2022-01-09 03:20:00 +0530

    Checking whether chain is valid or not

--- blockchain.py
+++ blockchain.py
@@ -59,6 +59,23 @@ class Blockchain:
         return hashlib.sha256(encoded_block).hexdigest()
     
     
+    def is_chain_valid(self,chain):
+        '''Checking whether the chain is valid or not'''
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            #if the previous hash value of block is not equal to the hash of previous block
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
     
     
     
